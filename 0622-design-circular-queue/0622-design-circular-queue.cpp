75932a4414ef06a958a0c7f7382a6a91febffed7
Saurabh667 <saurav000667@gmail.com>
2026-08-21 22:47:46 +0530

    Time: 6 ms (23.77%), Space: 23.5 MB (83.27%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.cpp
+++ 0622-design-circular-queue/0622-design-circular-queue.cpp
@@ -1,86 +1,80 @@
 class MyCircularQueue {
-    int *arr;
+    int* arr;
     int size;
     int front;
     int rear;
-public:
 
+public:
     MyCircularQueue(int k) {
-        size=k;
-        arr=new int[size];
-        front=rear=-1;
+        size = k;
+        arr = new int[size];
+        front = rear = -1;
     }
-    
+
     bool enQueue(int value) {
-        if((front==0 && rear==size-1) || (rear + 1) % size == front){
+        if ((front == 0 && rear == size - 1) || (rear + 1) % size == front) {
             // cout << "queue is full";
             return false;
-        }
-        else if(front==-1){
-            front=rear=0;
-            arr[rear]=value;
+        } else if (front == -1) {
+            front = rear = 0;
+            arr[rear] = value;
             // rear++;
-        }
-        else if(front !=0 && rear==size-1){
-            rear=0;
-            arr[rear]=value;
+        } else if (front != 0 && rear == size - 1) {
+            rear = 0;
+            arr[rear] = value;
             // rear++;
-        }
-        else{
+        } else {
             rear++;
-            arr[rear]=value;
+            arr[rear] = value;
         }
         return true;
     }
-    
+
     bool deQueue() {
-        if(front==-1){
+        if (front == -1) {
             return false;
         }
-        int ans=arr[front];
-        arr[front]=-1;
-        if(front==rear){
-            front=rear=-1;
-        }
-        else if(front==size-1){
-            front=0;
-        }
-        else{
+        int ans = arr[front];
+        arr[front] = -1;
+        if (front == rear) {
+            front = rear = -1;
+        } else if (front == size - 1) {
+            front = 0;
+        } else {
             front++;
         }
         // return ans;
         return true;
     }
-    
+
     int Front() {
-        if (front == -1){
+        if (front == -1) {
 
             return -1;
         }
         return arr[front];
     }
-    
+
     int Rear() {
         if (rear == -1) {
-    return -1;
-}
+            return -1;
+        }
         return arr[rear];
     }
-    
+
     bool isEmpty() {
-        if(front==-1){
+        if (front == -1) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
-    
+
     bool isFull() {
-        if((front==0 && rear==size-1) || (rear + 1) % size == front){
+        if ((front == 0 && rear == size - 1) || (rear + 1) % size == front) {
             return true;
         }
-            return false;
+        return false;
     }
 };
 

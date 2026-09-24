24cb597aac70cb4032579e79ff6a23ac024a0bc0
Saurabh667 <saurav000667@gmail.com>
2026-09-24 15:29:31 +0530

    Time: 3 ms (38.05%), Space: 8.8 MB (35.3%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        // int steps=0;
+        int steps=0;
         int cap=1;
         while(cap<=n){
             n=n-cap;
-            // steps++;
+            steps++;
             cap++;
         }
         return cap-1;
